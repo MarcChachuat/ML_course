987db4927df6cf10b4dbae73289885891df95d0f
Chachuat Marc <marc.chachuat@gmail.com>
2016-10-15 13:45:43 +0200

    td 4 : done
    td 4 completed

--- labs/ex04/template/ridge_regression.py
+++ labs/ex04/template/ridge_regression.py
@@ -5,12 +5,17 @@ Ridge Regression
 """
 
 import numpy as np
+from costs import *
 
 
 def ridge_regression(y, tx, lamb):
     """implement ridge regression."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # ridge regression: TODO
-    # ***************************************************
-    raise NotImplementedError
+    #define an auxiliary variable
+    lamb_aux=2*len(y)*lamb
+    # analytical solution
+    gram= np.dot(np.transpose(tx),tx)
+    w_ridge= np.dot(np.dot(np.linalg.inv(gram+lamb_aux*np.identity(gram.shape[0])),np.transpose(tx)),y)
+    # calculate the error (cost function)
+    rr_cost= compute_loss(y,tx,w_ridge)+lamb*(np.linalg.norm(w_ridge)**2)
+    # return the cost and the optimal weight
+    return rr_cost, w_ridge
--- labs/ex04/template/split_data.py
+++ labs/ex04/template/split_data.py
@@ -13,7 +13,16 @@ def split_data(x, y, ratio, seed=1):
     # set seed
     np.random.seed(seed)
     # ***************************************************
-    # INSERT YOUR CODE HERE
     # split the data based on the given ratio: TODO
-    # ***************************************************
-    raise NotImplementedError
+    indices=np.arange(x.shape[0])
+    np.random.shuffle(indices)
+    
+    #upperbound for the ratio : 
+    up=int(ratio * x.shape[0])
+    #creation of the data sets
+    xtrain=x[indices[0:up]]
+    xtest=x[indices[up:]]
+    ytrain=y[indices[0:up]]
+    ytest=y[indices[up:]]
+    #returning the result
+    return xtrain,xtest,ytrain,ytest
